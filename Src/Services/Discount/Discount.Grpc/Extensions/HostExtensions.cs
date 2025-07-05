5f5dd9a6c4713e16efaf9183d66c9d31f359543c
Fazeli1988 <fazeli.workbox@gmail.com>
2025-07-05 12:32:34 +0200

    Add GRPC Discount project and confing this project and write CRUD for discount project

---
+++ Src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability=retry.Value;
+            using (var scope = host.Services.CreateScope())
+            {
+                var sevices = scope.ServiceProvider;
+                var configuration= sevices.GetRequiredService<IConfiguration>();
+                var logger=sevices.GetRequiredService<ILogger<TContext>>();
+
+                //migrate database
+                try
+                {
+                    logger.LogInformation("migrating postgresql database");
+                    using var connection = new NpgsqlConnection
+               (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon (Id SERIAL PRIMARY KEY,
+                                                                 ProductName VARCHAR(200) NOT NULL,
+                                                                 Description TEXT,
+                                                                 Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    //seed data 
+                    command.CommandText = "INSERT INTO Coupon(ProductName,Description,Amount) VALUES('Iphone x','Iphone Discount',150)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName,Description,Amount) VALUES('Sumsung 10','Sumsung Discount',150)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("migration has been completed!!!");
+                }
+
+                catch (NpgsqlException ex) 
+                {
+                    logger.LogError("an errore has been occured");
+                    if (retryForAvailability < 50) 
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+
+                    }
+                }
+            }
+            return host;    
+        }
+       
+    }
+}
---
+++ Src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,17 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(StartupBase));
+var app = builder.Build();
+
+app.MapGrpcService<DiscountRepository>();
+// Configure the HTTP request pipeline.
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MigrateDatabase<Program>();
+
+app.Run();
