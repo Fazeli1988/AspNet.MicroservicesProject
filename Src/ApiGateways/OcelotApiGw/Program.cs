9ee5c429e4142953c24de67c420b932457b26d29
fazeli1988 <fazeli.workbox@gmail.com>
2025-09-03 19:35:47 +0200

    created response cache containerize ocelotGetway on docker

--- Src/ApiGateways/OcelotApiGw/Program.cs
+++ Src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,9 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services
+    .AddOcelot()
+    .AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
